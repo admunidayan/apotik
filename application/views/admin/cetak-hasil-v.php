6cf7dd58e9a654a511d0040ba29265920bf466c4
admunidayan <35984858+admunidayan@users.noreply.github.com>
2018-02-28 23:43:41 +0700

    ubah tampilan print

--- application/views/admin/cetak-hasil-v.php
+++ application/views/admin/cetak-hasil-v.php
@@ -9,23 +9,23 @@
 			<td colspan="2" style="text-align: center;border:1px solid"><h1>Hasil Pemasaran pada tanggal <?php echo $tanggal; ?></h1></td>
 		</tr>
 		<tr>
-			<td width="50%" style="vertical-align: top; border-left: 1px solid; padding: 7px">
+			<td width="50%" style="vertical-align: top; border-left: 1px solid;">
 				<h5 class="text-info">Daftar Nota</h5>
 				<table width="100%">
 					<tr>
-						<td>No</td>
-						<td>Nota</td>
-						<td>Kasir</td>
-						<td>Total</td>
+						<td style="border-bottom: 1px solid">No</td>
+						<td style="border-bottom: 1px solid;border-left: 1px solid">Nota</td>
+						<td style="border-bottom: 1px solid;border-left: 1px solid">Kasir</td>
+						<td style="border-bottom: 1px solid;border-left: 1px solid">Total</td>
 					</tr>
 					<?php $harga_nota = 0 ?>
 					<?php $no = 1 ?>
 					<?php foreach ($hasil as $data): ?>
 						<tr>
-							<td><?php echo $no; ?></td>
-							<td><?php echo $data->id_nota; ?></td>
-							<td><?php echo $data->username ?></td>
-							<td><?php echo 'Rp.'.number_format($data->total_bayar_nota,0,',','.') ?></td>
+							<td style="border-bottom: 1px solid"><?php echo $no; ?></td>
+							<td style="border-bottom: 1px solid;border-left: 1px solid"><?php echo $data->id_nota; ?></td>
+							<td style="border-bottom: 1px solid;border-left: 1px solid"><?php echo $data->username ?></td>
+							<td style="border-bottom: 1px solid;border-left: 1px solid"><?php echo 'Rp.'.number_format($data->total_bayar_nota,0,',','.') ?></td>
 						</tr>
 						<?php $harga_nota = $data->total_bayar_nota + (int)@$harga_nota; ?>
 						<?php $no++ ?>
@@ -38,18 +38,18 @@
 				<h5 class="text-info">Daftar Pemasukan</h5>
 				<table width="100%">
 					<tr>
-						<td>No</td>
-						<td>Keterangan</td>
-						<td>Total</td>
+						<td style="border-bottom: 1px solid">No</td>
+						<td style="border-bottom: 1px solid;border-left: 1px solid">Keterangan</td>
+						<td style="border-bottom: 1px solid;border-left: 1px solid">Total</td>
 					</tr>
 					<?php if ($umasuk == TRUE): ?>
 						<?php $harga_masuk = 0 ?>
 						<?php $no = 1 ?>
 						<?php foreach ($umasuk as $data): ?>
 							<tr>
-								<td><?php echo $no; ?></td>
-								<td><?php echo $data->keterangan; ?></td>
-								<td><?php echo 'Rp.'.number_format($data->jumlah,0,',','.') ?></td>
+								<td style="border-bottom: 1px solid"><?php echo $no; ?></td>
+								<td style="border-bottom: 1px solid;border-left: 1px solid"><?php echo $data->keterangan; ?></td>
+								<td style="border-bottom: 1px solid;border-left: 1px solid"><?php echo 'Rp.'.number_format($data->jumlah,0,',','.') ?></td>
 							</tr>
 							<?php $harga_masuk = $data->jumlah + (int)@$harga_masuk; ?>
 							<?php $no++ ?>
@@ -67,18 +67,18 @@
 				<h5 class="text-info">Daftar Pengeluaran</h5>
 				<table width="100%">
 					<tr>
-						<td>No</td>
-						<td>Keterangan</td>
-						<td>Total</td>
+						<td style="border-bottom: 1px solid">No</td>
+						<td style="border-bottom: 1px solid;border-left: 1px solid">Keterangan</td>
+						<td style="border-bottom: 1px solid;border-left: 1px solid">Total</td>
 					</tr>
 					<?php if ($ukeluar == TRUE): ?>
 						<?php $harga_keluar = 0 ?>
 						<?php $no = 1 ?>
 						<?php foreach ($ukeluar as $data): ?>
 							<tr>
-								<td><?php echo $no; ?></td>
-								<td><?php echo $data->keterangan; ?></td>
-								<td><?php echo 'Rp.'.number_format($data->jumlah,0,',','.') ?></td>
+								<td style="border-bottom: 1px solid"><?php echo $no; ?></td>
+								<td style="border-bottom: 1px solid;border-left: 1px solid"><?php echo $data->keterangan; ?></td>
+								<td style="border-bottom: 1px solid;border-left: 1px solid"><?php echo 'Rp.'.number_format($data->jumlah,0,',','.') ?></td>
 							</tr>
 							<?php $harga_keluar = $data->jumlah + (int)@$harga_keluar; ?>
 							<?php $no++ ?>
@@ -94,20 +94,20 @@
 					<?php endif ?>
 				</table>
 			</td>
-			<td width="50%" style="vertical-align: top;border-right: 1px solid;border-left: 1px solid;padding: 7px">
+			<td width="50%" style="vertical-align: top;border-right: 1px solid;border-left: 1px solid;">
 				<h5 class="text-info">Jumlah Barang Terjual</h5>
 				<table width="100%">
 					<tr>
-						<td>No</td>
-						<td>Nama Barang</td>
-						<td>Jml</td>
+						<td style="border-bottom: 1px solid">No</td>
+						<td style="border-bottom: 1px solid;border-left: 1px solid">Nama Barang</td>
+						<td style="border-bottom: 1px solid;border-left: 1px solid">Jml</td>
 					</tr>
 					<?php $no = 1 ?>
 					<?php foreach ($brglaku as $barang): ?>
 						<tr>
-							<td><?php echo $no; ?></td>
-							<td><?php echo strtoupper($barang->nama_menu); ?></td>
-							<td><?php echo $barang->jml_laku; ?></td>
+							<td style="border-bottom: 1px solid"><?php echo $no; ?></td>
+							<td style="border-bottom: 1px solid;border-left: 1px solid"><?php echo strtoupper($barang->nama_menu); ?></td>
+							<td style="border-bottom: 1px solid;border-left: 1px solid"><?php echo $barang->jml_laku; ?></td>
 						</tr>
 						<?php $no++ ?>
 					<?php endforeach ?>
@@ -115,24 +115,24 @@
 				<h5 class="text-info">Total Penghasilan</h5>
 				<table width="100%">
 					<tr>
-						<td>No</td>
-						<td>Asal</td>
-						<td>Jumlah</td>
+						<td style="border-bottom: 1px solid">No</td>
+						<td style="border-bottom: 1px solid;border-left: 1px solid">Asal</td>
+						<td style="border-bottom: 1px solid;border-left: 1px solid">Jumlah</td>
 					</tr>
 					<tr>
-						<td>1</td>
-						<td>Uang Nota</td>
-						<td><?php echo 'Rp.'.number_format(@$harga_nota,0,',','.'); ?></td>
+						<td style="border-bottom: 1px solid">1</td>
+						<td style="border-bottom: 1px solid;border-left: 1px solid">Uang Nota</td>
+						<td style="border-bottom: 1px solid;border-left: 1px solid"><?php echo 'Rp.'.number_format(@$harga_nota,0,',','.'); ?></td>
 					</tr>
 					<tr>
-						<td>2</td>
-						<td>Uang Masuk</td>
-						<td><?php echo 'Rp.'.number_format(@$harga_masuk,0,',','.'); ?></td>
+						<td style="border-bottom: 1px solid">2</td>
+						<td style="border-bottom: 1px solid;border-left: 1px solid">Uang Masuk</td>
+						<td style="border-bottom: 1px solid;border-left: 1px solid"><?php echo 'Rp.'.number_format(@$harga_masuk,0,',','.'); ?></td>
 					</tr>
 					<tr>
-						<td>3</td>
-						<td>Uang Keluar</td>
-						<td><?php echo 'Rp.'.number_format(@$harga_keluar,0,',','.'); ?></td>
+						<td style="border-bottom: 1px solid">3</td>
+						<td style="border-bottom: 1px solid">Uang Keluar</td>
+						<td style="border-bottom: 1px solid"><?php echo 'Rp.'.number_format(@$harga_keluar,0,',','.'); ?></td>
 					</tr>
 					<tr>
 						<td colspan="2"><b>Total</b></td>
